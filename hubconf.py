73d62b2343a2d2ac4cf07935aa90921bfbf3f7f8
r4m-t1n <im.r4mt1n@gmail.com>
2025-07-04 22:35:05 +0330

    feat: adjust to words2idx pickle file

--- hubconf.py
+++ hubconf.py
@@ -1,16 +1,24 @@
-import torch
 import os
+import pickle
+import torch
 from src.model import SentimentBinaryClassifier
-from src.data_loader import preprocess_text, text_to_indices, IMDBDataset, TRAIN_DIR
+from src.data_loader import preprocess_text, text_to_indices
 from src.predict import predict_text
 
 dependencies = ['numpy==1.24.3', 'scikit-learn==1.4.2', 'torch==2.3.0']
 
 MODEL_PATH = os.path.join(os.path.dirname(__file__), 'models', 'sentiment_model.pt')
+WORDS_PATH = os.path.join(os.path.dirname(__file__), 'models', 'words2idx.pickle')
+
+def _load_vocab():
+    if not os.path.exists(WORDS_PATH):
+        raise FileNotFoundError(f"Vocabulary file not found at {WORDS_PATH}. ")
+    with open(WORDS_PATH, 'rb') as f:
+        words2idx = pickle.load(f)
+    return words2idx
 
 def sentiment_classifier(pretrained=True, device=None, **kwargs):
-    temp_train = IMDBDataset(TRAIN_DIR)
-    words2idx = IMDBDataset.build_vocab(temp_train.samples)
+    words2idx = _load_vocab()
     vocab_size = len(words2idx)
 
     if device is None:
