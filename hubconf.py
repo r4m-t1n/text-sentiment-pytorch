013ab93a36cb9b0e4844ceb92056628ba8145c49
r4m-t1n <im.r4mt1n@gmail.com>
2025-07-04 23:01:29 +0330

    feat: adjust to words2idx pickle file

--- hubconf.py
+++ hubconf.py
@@ -1,9 +1,8 @@
 import os
+import re
 import pickle
 import torch
 from src.model import SentimentBinaryClassifier
-from src.data_loader import preprocess_text, text_to_indices
-from src.predict import predict_text
 
 dependencies = ['numpy==1.24.3', 'scikit-learn==1.4.2', 'torch==2.3.0']
 
@@ -17,9 +16,10 @@ def _load_vocab():
         words2idx = pickle.load(f)
     return words2idx
 
+words2idx = _load_vocab()
+vocab_size = len(words2idx)
+
 def sentiment_classifier(pretrained=True, device=None, **kwargs):
-    words2idx = _load_vocab()
-    vocab_size = len(words2idx)
 
     if device is None:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -36,6 +36,40 @@ def sentiment_classifier(pretrained=True, device=None, **kwargs):
 
     return model
 
-predict = predict_text
-preprocess = preprocess_text
-text2idx = text_to_indices
+def predict_text(model, text, word2idx, device):
+
+    processed_text = preprocess_text(text)
+
+    indices = text_to_indices(processed_text, word2idx)
+    
+    if not indices:
+        print("Warning: Input text became empty after preprocessing.")
+        return "Empty", 0.5
+
+    text_tensor = torch.tensor(indices, dtype=torch.long).unsqueeze(0).to(device)
+    
+    with torch.no_grad():
+        output = model(text_tensor)
+
+    probability = output.item()
+
+    prediction = (probability >= 0.5)
+    sentiment = "(+)Positive" if prediction else "(-)Negative"
+    
+    return sentiment, probability
+
+def preprocess_text(text):
+    text = re.sub(r'<.*?>', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
+    text = text.lower()
+    text = re.sub(r'[^a-z0-9\s.,!?;:]', '', text) 
+    return text.strip()
+
+def text_to_indices(text, word2idx):
+    indices = []
+    for word in text.split():
+        if word in word2idx:
+            indices.append(word2idx[word])
+        else:
+            indices.append(word2idx['<UNK>'])
+    return indices
